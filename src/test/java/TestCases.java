6907dbf673e5caa03f03085839e78ee24c87a983
Musfiqur Rahman <musfiqurrahman1205@gmail.com>
2024-03-12 12:06:56 +0600

    Initial commit

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+    @Test
+    void Test1()
+    {
+        System.out.println("i am test");
+    }
+    @Test
+    void Test2()
+    {
+        System.out.println("i am test");
+    }
+    @Test
+    void Test3()
+    {
+        System.out.println("i am test");
+    }
+}
